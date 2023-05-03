1df190a6c29e523ad9c8087430fd8e027e14c019
genga898 <genga898@gmail.com>
2023-05-03 13:03:55 +0300

    Update the pageload function
    Pulls data from the database and dsplays it in a card

--- Users/fruits.aspx
+++ Users/fruits.aspx
@@ -5,6 +5,8 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css">
     <script defer src="https://use.fontawesome.com/releases/v5.7.2/css/all.css"></script>
     <script defer src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
     <div class="container-fluid">
@@ -16,17 +18,17 @@
                 <div class="col-sm-4 col-md-4 col-lg-2">
                     <div class="card">
                         <div class="card-img-top">
-                            <asp:Image runat="server" ID="imgProduct" CssClass="card-img-fixed-height" ImageUrl="~/images/cheese.jpg" />
+                            <asp:Image runat="server" ID="imgProduct" CssClass="card-img-fixed-height" ImageUrl='<%# Eval("ImageUrl") %>' />
                         </div>
                         <div class="card-body">
-                            <h5 class="card-title"><asp:Literal runat="server" ID="litProductName" Text="Product Name" /></h5>
-                            <p class="card-text"><asp:Literal runat="server" ID="litProductDescription" Text="Product Description" /></p>
+                            <h5 class="card-title">"<%# Eval("name") %>"</h5>
+                            <p class="card-text"><asp:Literal runat="server" ID="litProductDescription" Text="Fruits" /></p>
                             <div class="row">
                                 <div class="col-6">
-                                    <p><asp:Literal runat="server" ID="litProductPrice" Text="$10.99" /></p>
+                                    <p>Ksh: "<%# Eval("price") %>"</p>
                                 </div>
                                 <div class="col-6">
-                                    <asp:Button runat="server" ID="btnAddToCart" Text="Add to Cart" CssClass="btn btn-outline-success float-right" />
+                                    <asp:Button runat="server" ID="btnAddToCart" Text="Add to Cart" CssClass="btn btn-outline-success float-right" OnClick="btnAddToCart_OnClick"/>
                                 </div>
                             </div>
                         </div>
--- Users/fruits.aspx.cs
+++ Users/fruits.aspx.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using GroceriesWebApp.Users;
 
 namespace GroceriesWebApp
 {
@@ -11,6 +14,78 @@ namespace GroceriesWebApp
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            Database.DbConnect();
+            if (!IsPostBack)
+            {
+                GetProducts();
+            }
+        }
+
+        protected void btnAddToCart_OnClick(object sender, EventArgs e)
+        {
+            if (Session["UserID"] == null)
+            {
+                ClientScript.RegisterClientScriptBlock(this.GetType(), "Redirect", "Toastify({\\r\\ntext: \\\"Login Required before adding to cart\\\",\\r\\nduration: 3000,\\r\\nclose: true,\\r\\ngravity: \\\"top\\\",\\r\\nposition: \\\"right\\\",\\r\\nstopOnFocus: false,\\r\\nstyle: {\\r\\n background: \\\"red\\\",\\r\\n}\\r\\n}).showToast();\" +\r\n\"" +
+                    "window.location.href = 'login.aspx'");
+            }
+            else
+            {
+                SqlCommand cmd;
+                int userID = Convert.ToInt32(Session["UserID"]),
+                    quantity = 1,
+                    productID = 0;
+                string toast;
+
+
+                string sqlText = "Insert into Cart(Product_id, Quantity, UserId)" +
+                                 " values(@prod_id, @quantity, @user_ID)";
+                cmd = new SqlCommand();
+                cmd.CommandText = sqlText;
+                cmd.Connection = Database.Conn;
+                /*DataView dv = (DataView)sdsFruits.Select(DataSourceSelectArguments.Empty);
+                if (dv != null && dv.Count > 0)
+                {
+                    productID = (int)dv[0]["product_id"];
+                    cmd.Parameters.AddWithValue("@prod_id", productID);
+                }*/
+                cmd.Parameters.AddWithValue("@quantity", quantity);
+                cmd.Parameters.AddWithValue("@user_ID", userID);
+
+                cmd.ExecuteNonQuery();
+                cmd.Dispose();
+                Database.Conn.Close();
+                toast = "Toastify({\r\n            " +
+                        "text: \"Item Successfully added to cart\",\r\n" +
+                        "duration: 3000,\r\n" +
+                        "close: true,\r\n" +
+                        "gravity: \"top\",\r\n" +
+                        "position: \"center\",\r\n" +
+                        "style: " +
+                        "{\r\n"+
+                        "     background: \"green\"\r\n" +
+                        "}\r\n\r\n" +
+                        "}).showToast();";
+                ClientScript.RegisterClientScriptBlock(this.GetType(), "Alert", toast, true);
+            }
+        }
+
+        protected void GetProducts()
+        {
+            DataTable dt;
+            SqlCommand cmd;
+            SqlDataAdapter dataAdapter;
+            string sqlText = "Select product_id, name, price, quantity, ImageUrl, Category_id, created_at " +
+                             "from Products " +
+                             "where quantity > 0 and Category_id = 2 " +
+                             "order by created_at;";
+            cmd = new SqlCommand();
+            cmd.Connection = Database.Conn;
+            cmd.CommandText = sqlText;
+            dataAdapter = new SqlDataAdapter(cmd);
+            dt = new DataTable();
+            dataAdapter.Fill(dt);
+            rpFruits.DataSource = dt;
+            rpFruits.DataBind();
 
         }
     }
